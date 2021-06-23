074d035e7a7f27ae68925307fe28b45b3009fb53
nojaja <free.riccia@gmail.com>
2021-06-24 02:12:24 +0900

    データ取得部分をdaoに分離

--- src/js/index.js
+++ src/js/index.js
@@ -2,36 +2,10 @@
 import OrgChart from 'orgchart';
 import 'orgchart/dist/css/jquery.orgchart.min.css'
 import '../css/style.css'
-import initSqlJs from "sql.js";
 import Handlebars from "handlebars";
+import Dao from "./dao.js";
 
-/**
- * GB929F_Functional_Framework_Functional_Decomposition_v21.0.0.xlsx
- * GB921_Business_Process_Framework_Processes_v21.0.0.xlsx
- * https://docs.google.com/spreadsheets/d/1P4WQxQnO-gbPkVZ2d85T9I0dClw9Lm3rQ_cvSmyYz88/edit#gid=0
- * 
- * DDL：
- * CREATE TABLE TAM (ID PRIMARY KEY, LEVEL integer, PARENT , NAME , TYPE , ORIGINAL_ID , DESCRIPTION , FUNCTION , BRIEF_DESCRIPTION , DOMAIN , CATEGORY , MATURITY_LEVEL , FRAMEWORX_STATUS );
- * CREATE TABLE ETOM (ID PRIMARY KEY, LEVEL integer, PARENT , NAME , TYPE , ORIGINAL_ID , DESCRIPTION , FUNCTION , BRIEF_DESCRIPTION , DOMAIN , CATEGORY , MATURITY_LEVEL , FRAMEWORX_STATUS );
- * 
- * FRAMEWORX_STATUS INT from "Frameworx Status" val => "Released":1 other:2
- * ID TEXT from "Application Identifier" 
- * DOMAIN from "Application Identifier" level1 val => "3.2": 3
- * PARENT from  "Application Identifier" level1 val => "3.2.1": "3.2"
- * 
- * DML sample:
- * INSERT INTO ETOM VALUES ('1.1.1','2','1.1','Market Strategy & Policy','(2) eTOM Process Type','1.2.1.1','Market Strategy & Policy processes enable the development of a strategic view of an enterprise’s existing and desired market-place, activities and aims. Market segmentation and analysis is performed, to determine an enterprise’s target and addressable markets, along with the development of marketing strategies for each market segment or set of target customers. The decision is made as to which markets the enterprise wants or needs to be in, and how it plans to enter or grow in these markets and market segments. This will be achieved through multiple inputs: including Enterprise Strategies, Market Research, Market Analysis.','null','Enable the development of a strategic view of an enterprise’s existing and desired market-place','Market Sales Domain','Strategy Management','4','Released');
- * INSERT INTO TAM VALUES ('3.2','1','3','Sales Aids','(1) TAM Application Type','null','The Sales Aids Application provides access to methods and procedures as well as product information and other collateral that can be used to assist in making a sale.','','null','Market Sales Domain','null','4','1');
- */
-
-//SQLiteの設定
-const config = {
-  // Required to load the wasm binary asynchronously. Of course, you can host it wherever you want
-  // You can omit locateFile completely when running in node
-  //locateFile: file => `https://sql.js.org/dist/${file}`
-  //locateFile: filename => `/dist/${filename}`
-  locateFile: file => './sql-wasm.wasm'
-}
+const dao = new Dao()
 
 //GETパラメータの取得
 const getParam = function () {
@@ -44,18 +18,8 @@ const getParam = function () {
   return arg
 }
 
-//SQLiteの読み込み
-// return {Promise} db
-// ex. await db
-const db = async function () {
-  const sqlPromise = initSqlJs(config);
-  const dataPromise = fetch("./assets/Frameworx_DB_Model_21.0.db").then(res => res.arrayBuffer());
-  const [SQL, buf] = await Promise.all([sqlPromise, dataPromise])
-  return new SQL.Database(new Uint8Array(buf));
-}()
 
 let oc; //階層チャート
-
 //View///////////////////////////////////////////////////
 //コンテンツロード完了イベントのハンドリング
 document.addEventListener('DOMContentLoaded', function () {
@@ -98,12 +62,11 @@ window.addEventListener('popstate', (event) => {
   pageGen(type, id);
 });
 
-const TABLES = ['TAM', 'ETOM', 'ODA_Functional_Blocks']
 const pageCache = {}
 
 //ページの描画処理
 let pageGen = async function (type, id) {
-  const table = TABLES[type]
+  const table = dao.TABLES[type]
 
   let getPageTemplate = async function (table) {
     //table単位でページテンプレートを読み込み
@@ -114,12 +77,12 @@ let pageGen = async function (type, id) {
     return pageCache[table]
   }
 
-  const data = await getData(table, id)
-  getPageTemplate(table).then(template => {
-    data.chartContainer = oc.init({ 'data': data }).$chartContainer[0]
-    document.getElementById("content_body").innerHTML = template({ data: data })
-    document.getElementById("chart-container").appendChild(data.chartContainer)
+  const data = await dao.getData(table, id)
+  const template = await getPageTemplate(table)
 
+  document.getElementById("content_body").innerHTML = template({ data: data })
+  data.chartContainer = oc.init({ 'data': data }).$chartContainer[0]
+  document.getElementById("chart-container").appendChild(data.chartContainer)
   const elements = document.getElementsByClassName("parentlink")
   for (let i = 0; i < elements.length; i++) {
     elements[i].onclick = (event) => {
@@ -130,80 +93,4 @@ let pageGen = async function (type, id) {
       pageGen(type, data.parent);
     }
   }
-  })
-}
-
-//ページのデータ取得
-let getData = async function (table, id) {
-  let db1 = await db
-  // Prepare an sql statement
-  const stmt = db1.prepare(`SELECT ID as name, NAME as title, DESCRIPTION as overview, MATURITY_LEVEL as maturity, FUNCTION as functionality, PARENT as parent, TYPE as category FROM ${table} WHERE ID=$id`);
-
-  // Bind values to the parameters and fetch the results of the query
-  //const result = stmt.getAsObject({'$id' : id});
-  // Bind new values
-  stmt.bind({ $id: id });
-  let datascource = {}
-  if (stmt.step()) { //primary keyで検索するので結果行は1:0
-    datascource = stmt.getAsObject();
-    datascource.children = await getChildData(table, id) //紐づく子供の取得
-    //datascource.relationData = {}
-    //for (const relationTable of TABLES) {
-    //  datascource.relationData[relationTable] = await getRelationData(relationTable,id)
-    //}
-    datascource.relationData = await getRelationData(table, id)
-  }
-  return datascource
-}
-
-//ページのに関連する子要素の取得
-let getChildData = async function (table, id) {
-  let db1 = await db
-  // Prepare an sql statement
-  const stmt = db1.prepare(`SELECT ID as name, NAME as title FROM ${table} WHERE PARENT=$id`);
-
-  // Bind values to the parameters and fetch the results of the query
-  //const result = stmt.getAsObject({'$id' : id});
-  // Bind new values
-  stmt.bind({ $id: id });
-  let children = [];
-  while (stmt.step()) { //
-    children.push(stmt.getAsObject())
-  }
-  return children
-}
-
-//ページのに関連する子要素の取得
-let getRelationData = async function (table, id) {
-  const result = {}
-  for (const relationTable of TABLES) {
-    if (table == relationTable) continue
-    const children = await getRelationChildData(table, id, relationTable)
-    if (children.length > 0)
-      result[relationTable] = children
-  }
-  return result
-}
-
-//ページのに関連する子要素の取得
-let getRelationChildData = async function (fromtable, fromid, totable) {
-  let db1 = await db
-  // Prepare an sql statement
-  const stmt = db1.prepare(`
-  SELECT ID as name, NAME as title FROM ${totable} WHERE ID IN(
-    SELECT TO_KEY as key FROM MAPPING WHERE TO_TABLE == $totable AND FROM_KEY == $id AND FROM_TABLE == $fromtable
-    UNION
-    SELECT FROM_KEY as key FROM MAPPING WHERE FROM_TABLE == $totable AND TO_KEY == $id AND TO_TABLE == $fromtable
-    )
-  `);
-
-  // Bind values to the parameters and fetch the results of the query
-  //const result = stmt.getAsObject({'$id' : id});
-  // Bind new values
-  stmt.bind({ $fromtable: fromtable, $id: fromid, $totable: totable });
-  let children = [];
-  while (stmt.step()) { //
-    children.push(stmt.getAsObject())
-  }
-  return children
 }
